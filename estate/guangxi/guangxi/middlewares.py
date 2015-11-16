2c5d1651a0a70b239051c2db4291b0d58df3f5d4
G_will <g_will@ieqi.com>
2015-11-16 16:50:15 +0800

    随机 ua 相关实现

---
+++ estate/guangxi/guangxi/middlewares.py
@@ -0,0 +1,12 @@
+# coding: utf-8
+
+import random
+
+from agents import AGENTS
+
+
+class CustomUserAgentMiddleware(object):
+
+    def process_request(self, request, spider):
+        agent = random.choice(AGENTS)
+        request.headers['User-Agent'] = agent
